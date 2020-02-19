11d0b2e88af90990389dc4b15f1cea5951fc652c
f1rpo <no.m@il.specified>
2020-02-19 23:08:17 +0100

    Revise ScaledInt class (to be continued)
    Prompted by comments by Nightinggale and devolution in this CFC thread:
    https://forums.civfanatics.com/threads/class-for-fixed-point-arithmetic.655037/
    (But not all comments have been addressed yet.)
    
    Turn static ScaledInt::szBuf from a char array into a CvString and move it to a base class because one instance shared by all ScaledInt types is enough.
    
    Rename static variables MIN and MAX to INTMIN, INTMAX to make clearer that they're the limits of IntType, not of ScaledInt. Now private. Add public functions MIN(), MAX() that return the limits of ScaledInt based on INTMIN and INTMAX.
    
    Changes to template parameters:
    * Replace 'class' with 'typename' in template parameter lists.
    * Rename the 'INT' parameter to 'IntType', in part, because INT is already defined (as an alias of int) in WinDef.h.
    * Switch the order of SCALE and IntType so that SCALE can be of type IntType. Known issue with this: Can't declare template classes with dependent arguments as friend; this appears to be a bug in the MSVC compiler:
    https://stackoverflow.com/questions/18666240/friending-a-dependent-argument-template
    I've made all private data members public until I can find a workaround.
    * Add a template parameter 'EnumType' to allow instantiations that are (on purpose; for type safety) incompatible with each other.
    * Remove unnecessary template argument lists.
    * Use preprocessor defines to make parameter/ argument lists easier to handle in global and out-of-class definitions.
    
    Define a type LongType depending on the size of IntType for intermediate results that are outside the limits of IntType. (Previously, __int64 was always used, which is wasteful when IntType has fewer than 4 bytes.)
    
    Add some more assertions to check for overflow and signed/unsigned mismatches.
    
    Add uint comparison operators. (Tbd.: Try using function templates instead.)
    
    Add serialization functions that read and write to FDataStreamBase.
    
    Fix an overflow bug in operator/= by forwarding to the toScale function.
    In the works: A mulDiv function template to handle operator*=, operator/= and scale conversions. Currently, there is some duplicate code, and operations on mixed scales aren't as accurate as they could be.

--- CvGameCoreDLL/ScaledIntTest.cpp
+++ CvGameCoreDLL/ScaledIntTest.cpp
@@ -7,6 +7,10 @@
 #ifdef SCALED_INT_TEST
 #include "TSCProfiler.h"
 #include "CvInfo_GameOption.h"
+
+// Can't define these in a function body (need to have linkage)
+TYPEDEF_SCALED_ENUM(int,166320,MovementPts) // Divisible by most numbers between 1 and 30
+TYPEDEF_SCALED_ENUM(int,1024,CurrCombatStr)
 #endif
 
 /*  To be called once XML data has been loaded. (Need some test data that is unknown
@@ -19,7 +23,7 @@ void TestScaledInt()
 
 	/*	These numbers match the running example commented on in pow.
 		(The example assumes scale 1024, hence the explicit constructor calls.) */
-	FAssert(ScaledInt<1024>(fixp(5.2)).pow(ScaledInt<1024>(fixp(2.1))).getInt() == 32);
+	FAssert((ScaledInt<int,1024>(fixp(5.2)).pow(ScaledInt<int,1024>(fixp(2.1))).round() == 32));
 
 	// Spotty unit test
 	FAssert((fixp(4/3.) * 1000).getInt() == 1333);
@@ -59,11 +63,18 @@ void TestScaledInt()
 	rTest.mulDiv(4, 5);
 	FAssert(rTest == 2);
 	FAssert(std::strcmp(scaled_int(2).str(100), "200 percent") == 0);
-	FAssert(std::strcmp(ScaledInt<1024>(2).str(), "2048/1024") == 0);
+	FAssert(std::strcmp(ScaledInt<int,1024>(2).str(), "2048/1024") == 0);
 	FAssert(std::strcmp(scaled_int(2).str(1), "2") == 0);
 	FAssert(std::strcmp(fixp(2.2).str(1), "ca. 2") == 0);
 	FAssert(scaled_int(42).roundToMultiple(5) == 40);
 	FAssert(scaled_int(-43).roundToMultiple(5) == -45);
+
+	MovementPts rMoves = fixp(1.5);
+	FAssert(rMoves == scaled_int(3, 2)); // Allowed: operations on ScaledInt<*,*,int>
+	FAssert(rMoves - 1 == MovementPts(1, 2));
+	CurrCombatStr rCombat = fixp(4.9);
+	// Not allowed: MovementPts and CurrCombatStr are incompatible w/ each other
+	//if (rMoves < rCombat) ;
 
 	/*	Will do something "non-const" at the end based on the value of iDummy.
 		To prevent the compiler from discarding code, results of test computations
