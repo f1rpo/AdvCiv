5ed2f154bf439d3c2f9ffae66c66396545b5b39e
f1rpo <no.m@il.specified>
2020-04-10 02:20:30 +0200

    Partly revert no-trading tech cost modifier tweak
    Amends SHA 8bdc222b9d23d8a26979035cfb9c6fbff42995f9

--- CvGameCoreDLL/CvTeam.cpp
+++ CvGameCoreDLL/CvTeam.cpp
@@ -2134,7 +2134,7 @@ int CvTeam::getResearchCost(TechTypes eTech, bool bGlobalModifiers, bool bTeamSi
 					GC.getDefineINT("TECH_COST_NOTRADE_MODIFIER"));
 			scaled rNoTradeAdjustment =
 					(rTECH_COST_NOTRADE_MODIFIER + per100(5) *
-					(eTechEra - scaled(3)).abs().pow(fixp(1.5))) *
+					(eTechEra - fixp(2.5)).abs().pow(fixp(1.5))) *
 					scaled::clamp(scaled(kWorld.getDefaultPlayers() - 2,
 					11 - eTechEra), 0, fixp(8/3.));
 			rNoTradeAdjustment.decreaseTo(0); // No Tech Trading can only lower tech costs
