05f1d8088cc84d42595227f37175868238aedf7a
f1rpo <no.m@il.specified>
2020-04-13 22:41:22 +0200

    Fix likely bug in BtS memory manager (unused?)

--- CvGameCoreDLL/CvMemoryManager.cpp
+++ CvGameCoreDLL/CvMemoryManager.cpp
@@ -112,7 +112,10 @@ void *__cdecl operator new(size_t size, char* pcFile, int iLine)
 
 void *__cdecl operator new[](size_t size, char* pcFile, int iLine)
 {
-	void* result = gDLL->newMem(size, pcFile, iLine);
+	FAssertMsg(false, "To check if operator new[](size_t*,char*,int) is ever used"); // advc.test
+	//void* result = gDLL->newMem(size, pcFile, iLine);
+	// advc.001: Looks like a copy-paste error
+	void* result = gDLL->newMemArray(size, pcFile, iLine);
 	memset(result, 0xDA, size); // advc.make
 	return result;
 }
@@ -124,7 +127,10 @@ void __cdecl operator delete(void* pvMem, char* pcFile, int iLine)
 
 void __cdecl operator delete[](void* pvMem, char* pcFile, int iLine)
 {
-	gDLL->delMem(pvMem, pcFile, iLine);
+	FAssertMsg(false, "To check if operator delete[](void*,char*,int) is ever used"); // advc.test
+	//gDLL->delMem(pvMem, pcFile, iLine);
+	// advc.001: Looks like a copy-paste error
+	gDLL->delMemArray(pvMem, pcFile, iLine);
 }
 
 
@@ -137,7 +143,7 @@ unsigned int memSize(void* a)
 {
 	return gDLL->memSize(a);
 }
-#endif // K-Mod
+#endif
 
 
 #ifdef MEMORY_TRACKING
@@ -252,7 +258,6 @@ CMemoryTrace::~CMemoryTrace()
 	if ( GetProcessMemoryInfo( GetCurrentProcess(), &pmc, sizeof(pmc)) )
 	{
 		char buffer[200];
-
 		sprintf(buffer, "function %s added %dK bytes, total now %dK\n", m_name, (int)(pmc.WorkingSetSize - m_start)/1024, (int)pmc.WorkingSetSize/1024);
 		OutputDebugString(buffer);
 	}
