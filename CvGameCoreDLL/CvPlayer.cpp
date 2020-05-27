2b107ca39dc70ccc4b70091be1e39f0a46c00d31
f1rpo <no.m@il.specified>
2020-05-27 23:49:32 +0200

    Disable assertion about AIs on strike during anarchy
    AI civs may lose units to a strike when one more than 1 turn of anarchy. That's a (minor) known BtS issue. I've kept this assertion for a while to see how frequently that happens (quite rarely in my games).

--- CvGameCoreDLL/CvPlayer.cpp
+++ CvGameCoreDLL/CvPlayer.cpp
@@ -13256,7 +13256,7 @@ void CvPlayer::doGold()
 		{
 			int iDisbandUnit = (getStrikeTurns() / 2); // XXX mod?
 			// advc.131: Can happen, but should be quite rare.
-			FAssert(isHuman());
+			FAssert(isHuman() || isAnarchy());
 			for (int iI = 0; iI < iDisbandUnit; iI++)
 			{
 				disbandUnit(true);
