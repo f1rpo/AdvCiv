5a2cfea8e6986442118aa6c59a89747d189efa87
f1rpo <no.m@il.specified>
2020-12-12 12:05:25 +0100

    Put a cap on the displayed excess garrison strength
    Relevant when a player has a large stack in a city with high foreign culture strength. Large numbers are unhelpful, difficult to read, can be confusing. [advc.101]

--- CvGameCoreDLL/CvGameTextMgr.cpp
+++ CvGameCoreDLL/CvGameTextMgr.cpp
@@ -4543,7 +4543,7 @@ void CvGameTextMgr::setPlotHelp(CvWStringBuffer& szString, CvPlot const& kPlot)
 						{
 							szString.append(gDLL->getText(
 									"TXT_KEY_GARRISON_STRENGTH_EXCESS_SHORT",
-									iSafeToRemove));
+									std::min(999, iSafeToRemove)));
 							szString.append(NEWLINE);
 						}
 					}
